c5b73e4263b055eef1496c73565fa5b6dab05b16
BlackDragon-B <kyan2007@live.nl>
2024-12-15 21:41:42 +0100

    boilerplate voor winkelwagen en migration om het leven iets simpeler te maken

---
+++ helpers/winkelwagen.php
@@ -0,0 +1,76 @@
+<?php
+    function mutate_product($dbcon, $gebruikerid, $productid, $amount) {
+        /*
+        Muteer een product in de winkelwagen database
+        accepteert een databaseconnectie, gebruikerid, productid en amount als argumenten, amount argument werkt als volgt:
+        "remove": Item wordt geupdate zodat huidige aantal 1 minder is
+        "append": Item wordt geupdate zodat huidige aantal 1 meer is. Als het item niet bestaat vind er een INSERT statement plaats ipv update
+        anders: aantal in tabel wordt naar $amount aangepast
+        returnt een boolean die aangeeft of de operatie geslaagd is
+        */
+        if (!empty($dbcon) && !empty($gebruikerid) && !empty($productid) && !empty($amount)) {
+            $gebruikerid = $dbcon->real_escape_string($gebruikerid);
+            $productid = $dbcon->real_escape_string($productid);
+            $amount = $dbcon->real_escape_string($amount);
+
+            if ($amount == "remove") {
+                $query = "UPDATE winkelwagen SET aantal = aantal - 1 WHERE gebruiker_id = '" . $gebruikerid . "' AND product_id = '" . $productid . "'"; 
+            } else {
+                $result = $dbcon->query("SELECT id FROM winkelwagen WHERE gebruiker_id = '". $gebruikerid . "' AND product_id = '" . $productid . "'");
+                if ($result->num_rows == 0) {
+                    echo "ball";
+                    if ($amount == "append") { $amount = 1; }
+                    $query = "INSERT winkelwagen (gebruiker_id, laatst_geupdate, product_id, aantal) VALUES ('" . $gebruikerid . "', NOW(), '" . $productid . "', '" .  $amount . "')";
+                } else {
+                    $query = "UPDATE winkelwagen SET aantal = aantal + 1 WHERE gebruiker_id = '" . $gebruikerid . "' AND product_id = '" . $productid . "'"; 
+                }
+            }
+            $dbcon->query($query);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function get_winkelwagen($dbcon, $gebruikerid) {
+        /*
+        Haal de winkelwagen op
+        accepteert een databaseconnectie en gebruikerid als argumenten, returnt het volgende 
+        {
+            totale_prijs: Totale prijs
+            totale_korting: Totale prijs met korting
+            producten: {
+                productnaam:
+                prijs: prijs zonder korting (prijs*aantal)
+                korting: prijs met korting ((prijs/korting)*aantal)
+                aantal: Aantal producten in winkelwagen
+            }
+        }
+        // */
+        $gebruikerid = $dbcon->real_escape_string($gebruikerid);
+        if (!empty($dbcon) && !empty($gebruikerid)) {
+            $totaal = $dbcon->query("SELECT SUM(p.prijs*w.aantal) as totale_prijs, SUM((p.prijs*(p.kortingspercentage/100))*w.aantal) as totale_korting  from winkelwagen w INNER JOIN producten p ON w.product_id = p.id WHERE w.gebruiker_id = '" . $gebruikerid . "' GROUP BY gebruiker_id");
+            if ($totaal->num_rows > 0) {
+                $totaal = $totaal->fetch_assoc();
+                $producten = $dbcon->query("SELECT p.productnaam as productnaam, p.prijs*w.aantal as prijs, (p.prijs*(p.kortingspercentage/100))*w.aantal as korting, w.aantal from winkelwagen w INNER JOIN producten p ON w.product_id = p.id WHERE w.gebruiker_id = '" . $gebruikerid . "'");
+                return Array("totale_prijs" => $totaal["totale_prijs"], "totale_korting" => $totaal["totale_korting"], "producten" => $producten->fetch_assoc());    
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    function nuke_winkelwagen($dbcon, $gebruikerid) {
+        /*
+        Verwijder alles uit de winkelwagen
+        accepteert een databaseconnectie en gebruikerid als argumenten, returnt een boolean die aangeeft of de operatie geslaagd is
+        */
+        if (!empty($dbcon) && !empty($gebruikerid)) {
+            $dbcon->query("DELETE FROM winkelwagen WHERE gebruiker_id = '" . $dbcon->real_escape_string($gebruikerid) . "'");
+        } else {
+            return false;
+        }
+    }
+?>
