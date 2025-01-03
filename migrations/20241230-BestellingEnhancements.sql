5fad7fccf01f5df8d17801ded52d9739845014d5
Kyan <kyan@blackdrgn.nl>
2025-01-03 16:44:02 +0100

    kapotte migrations zijn erg grappig thx
    Yare Yare Daze

--- migrations/20241230-BestellingEnhancements.sql
+++ migrations/20241230-BestellingEnhancements.sql
@@ -1,4 +1,4 @@
 ALTER TABLE bestellingen
-ADD COLUMN track_and_trace VARCHAR(255),
-ADD COLUMN verzendmethode VARCHAR(255),
-ADD COLUMN betaalstatus VARCHAR(255),
+  ADD COLUMN track_and_trace VARCHAR(255),
+  ADD COLUMN verzendmethode VARCHAR(255),
+  ADD COLUMN betaalstatus VARCHAR(255);
