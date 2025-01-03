7b3ce635ed72d51ab89481506abeaa7b4a5ec07a
Kyan <kyan@blackdrgn.nl>
2025-01-03 17:01:37 +0100

    nog een brakke migration :P

--- migrations/20241230-ChangeFKBestelling_Product.sql
+++ migrations/20241230-ChangeFKBestelling_Product.sql
@@ -2,4 +2,4 @@ ALTER TABLE koppeltabel_bestelling_product
 DROP FOREIGN KEY koppeltabel_bestelling_product_ibfk_1;
 
 ALTER TABLE koppeltabel_bestelling_product
-    ADD CONSTRAINT fk_bestellingen FOREIGN KEY (bestellingen_id) REFERENCES bestellingen(id) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT fk_bestellingen FOREIGN KEY (bestelling_id) REFERENCES bestellingen(id) ON DELETE CASCADE ON UPDATE CASCADE;
